bfd399dc4180e3a4b796bcbda8e66701d6af68e2
Ben Murphy <benmurphy7@gmail.com>
2021-11-05 22:13:47 -0400

    Basic point system

--- bot.js
+++ bot.js
@@ -1,4 +1,7 @@
-const tmi = require('tmi.js');
+var tmi = require('tmi.js');
+var XMLHttpRequest = require('xhr2');
+
+const userMap = {};
 
 // Define configuration options
 const opts = {
@@ -23,15 +26,23 @@ client.on('connected', onConnectedHandler);
 // Connect to Twitch:
 client.connect();
 
+//Need to connect to client before running this, not sure how to hook the client connection to executing this
+//So just calling stuff at end of onConnectedHandler
+
+
 // Called every time the bot connects to Twitch chat
 function onConnectedHandler (addr, port) {
   console.log(`* Connected to ${addr}:${port}`);
+
+  rewardAll(100)
+  setInterval(function(){
+    rewardAll(100)}, 10000)
 }
 
 // Called every time a message comes in
 function onMessageHandler (target, context, msg, self) {
   if (self) { return; } // Ignore messages from the bot
-  
+
   if (channel == null) {
     channel = target
   }
@@ -44,24 +55,31 @@ function onMessageHandler (target, context, msg, self) {
   
   line = line.substring(1);
   const arr = line.split(' ');
-  const commandName = arr[0];
+  const cmd = arr[0];
 
   // If the command is known, let's execute it
-  if (commandName === 'dice') {
-    const num = rollDice(commandName);
+  if (cmd === 'dice') {
+    const num = rollDice(cmd);
     client.say(target, `You rolled a ${num}.`);
-    console.log(`* Executed ${commandName} command`);
+    console.log(`* Executed ${cmd} command`);
   } 
-  else if (commandName == 'buy')  {
+  else if (cmd == 'buy')  {
     print('buy cmd');
     buy(arr[1], arr[2])
   }
-  else if (commandName == 'sell') {
+  else if (cmd == 'sell') {
     print('sell cmd');
     sell(arr[1], arr[2])
   }
+  else if (cmd == 'pts' || cmd == 'points') {
+    var user = context.username
+    print(context)
+    var points = getUserBalance(user)
+    print(points)
+    say(`${context['display-name']} has ${points} points`)
+  }
   else {
-    console.log(`* Unknown command ${commandName}`);
+    console.log(`* Unknown command ${cmd}`);
   }
 }
 
@@ -70,9 +88,6 @@ function rollDice () {
   const sides = 20;
   return Math.floor(Math.random() * sides) + 1;
 }
-
-
-
 
 function buy(ticker, amount) {
   print("This worked?")
@@ -85,10 +100,77 @@ function sell(ticker, amount) {
 
 
 function say(msg) {
-  print(`saying ${msg}`)
+  print(`saying: ${msg}`)
   client.say(channel, msg);
 }
 
 function print(string) {
   console.log(string)
+}
+
+
+async function rewardAll(amount) {
+  var users = await getChatters();
+  // I will NOT remember this for looping over values...
+  for (var it = users.values(), user = null; user=it.next().value;) {
+    print(user)
+    var balance = 0;
+    if (user in userMap) {
+      print('user has it')
+      balance = userMap[user]
+      print(`current user balance ${balance}`)
+    }
+    balance += amount
+    userMap[user] = balance
+  }
+  say(`Giving all chatters ${amount} points`)
+}
+
+function getUserBalance(user) {
+  var balance = 0;
+  if (user in userMap) {
+    balance = userMap[user]
+  }
+  return balance
+}
+
+async function getChatters() {
+  var data = await makeRequest("GET", 'https://tmi.twitch.tv/group/user/traderbay/chatters');
+  var jsonResponse = JSON.parse(data);
+  var chatters = getAllValues(jsonResponse["chatters"])
+  return chatters
+}
+
+function makeRequest(method, url) {
+  return new Promise(function (resolve, reject) {
+      let xhr = new XMLHttpRequest();
+      xhr.open(method, url);
+      xhr.onload = function () {
+          if (this.status >= 200 && this.status < 300) {
+              resolve(xhr.response);
+          } else {
+              reject({
+                  status: this.status,
+                  statusText: xhr.statusText
+              });
+          }
+      };
+      xhr.onerror = function () {
+          reject({
+              status: this.status,
+              statusText: xhr.statusText
+          });
+      };
+      xhr.send();
+  });
+}
+
+function getAllValues(jsonObject) {
+  var valueSet = new Set()
+  for (var key in jsonObject) {
+    var valueArray = jsonObject[key];
+    valueArray.forEach(value => valueSet.add(value))
+  }
+
+  return valueSet;
 }
